0989c1c6d8612ce0f7b98da6b80fe71d964cc746
EslamKhder <eslamkhder81@gmail.com>
2021-02-01 19:08:27 +0200

    create JpaRepository Closes #7

---
+++ back-end/src/main/java/com/ecommerceapp/dao/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.ecommerceapp.dao;
+
+import com.ecommerceapp.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
