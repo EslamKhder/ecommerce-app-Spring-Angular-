ab2abfa2f0fefe7f6e611cb370dfbbe97c6aedce
EslamKhder <eslamkhder81@gmail.com>
2021-02-03 23:50:37 +0200

    create Category Componant Closes #22

--- front-end/angular-ecommerce/src/app/app.module.ts
+++ front-end/angular-ecommerce/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './componants/products/products.component';
+import { CategoryComponent } from './componants/category/category.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductsComponent
+    ProductsComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
