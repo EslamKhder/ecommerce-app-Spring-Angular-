92fc4104fe18d8f29f00ef4dd974f788b22019ee
EslamKhder <eslamkhder81@gmail.com>
2021-02-13 20:46:46 +0200

    impl computeCartTotals in cartService Closes #62

--- front-end/angular-ecommerce/src/app/services/cart-service.service.ts
+++ front-end/angular-ecommerce/src/app/services/cart-service.service.ts
@@ -7,6 +7,8 @@ import {CartItem} from '../model/cart-item';
 export class CartServiceService {
 
   cartItems : CartItem[] = [];
+  totalQuantity: number = 0;
+  totalPrice: number = 0;
 
   constructor() { }
 
@@ -32,6 +34,14 @@ export class CartServiceService {
   }
 
   private computeCartTotals() {
-
+    let totalQuantityValue: number = 0;
+    let totalPriceValue: number = 0;
+    for (let temp of this.cartItems) {
+      totalQuantityValue += temp.quantity;
+      // @ts-ignore
+      totalPriceValue += temp.quantity * temp.unitPrice;
+    }
+    this.totalQuantity = totalQuantityValue;
+    this.totalPrice = totalPriceValue;
   }
 }
