42ed80a4d8b5311bc4cae0ebb6eb413115842ed7
EslamKhder <eslamkhder81@gmail.com>
2021-02-17 09:04:01 +0200

    add annotation to model (State-country) Closes #83

--- back-end/src/main/java/com/ecommerceapp/model/Country.java
+++ back-end/src/main/java/com/ecommerceapp/model/Country.java
@@ -1,13 +1,22 @@
 package com.ecommerceapp.model;
 
+import lombok.Data;
+
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Data
+@Entity
+@Table(name = "country")
 public class Country extends BaseEntity{
 
+    @Column(name = "code")
     private String code;
 
+    @Column(name = "name")
     private String name;
 
-    Set<State> states = new HashSet<>();
+    @OneToMany(mappedBy = "country",fetch = FetchType.LAZY)
+    private Set<State> states;
 }
--- back-end/src/main/java/com/ecommerceapp/model/State.java
+++ back-end/src/main/java/com/ecommerceapp/model/State.java
@@ -1,8 +1,18 @@
 package com.ecommerceapp.model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "state")
 public class State extends BaseEntity{
 
+    @Column(name = "name")
     private String name;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "country_id")
     private Country country;
 }
