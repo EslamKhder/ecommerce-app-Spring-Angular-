fe027361bbc7bb3408ee0f607f672d512218da93
EslamKhder <eslamkhder81@gmail.com>
2021-02-17 10:09:59 +0200

    disable methods of JPA (Country - State) Closes #87

--- back-end/src/main/java/com/ecommerceapp/configure/ConfigureRepositoryRestConfiguration.java
+++ back-end/src/main/java/com/ecommerceapp/configure/ConfigureRepositoryRestConfiguration.java
@@ -1,7 +1,9 @@
 package com.ecommerceapp.configure;
 
 import com.ecommerceapp.model.Category;
+import com.ecommerceapp.model.Country;
 import com.ecommerceapp.model.Product;
+import com.ecommerceapp.model.State;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -13,11 +15,13 @@ public class ConfigureRepositoryRestConfiguration implements RepositoryRestConfi
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
         HttpMethod[] unsupportedMethod = {HttpMethod.GET,HttpMethod.DELETE,HttpMethod.PUT,HttpMethod.POST};
-        dispaleHttpMethod(Product.class,config,unsupportedMethod);
-        dispaleHttpMethod(Category.class,config,unsupportedMethod);
+        disableHttpMethod(Product.class,config,unsupportedMethod);
+        disableHttpMethod(Category.class,config,unsupportedMethod);
+        disableHttpMethod(Country.class,config,unsupportedMethod);
+        disableHttpMethod(State.class,config,unsupportedMethod);
     }
 
-    private void dispaleHttpMethod(Class theClass, RepositoryRestConfiguration config,HttpMethod[] unsupportedMethod){
+    private void disableHttpMethod(Class theClass, RepositoryRestConfiguration config,HttpMethod[] unsupportedMethod){
         config.getExposureConfiguration()
                 .forDomainType(theClass)
                 .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedMethod))
